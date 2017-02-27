8e46bb94fb33b84f383e9d6774c2d9daac7e886c
Jesus Carabano <jcaraban@abo.fi>
2017-02-27 21:06:53 +0200

    ifdef ndef for F_ALLOC_

--- file/binary.cpp
+++ file/binary.cpp
@@ -389,8 +389,13 @@ Ferr binary::writeBlock(const Block &block) {
 	return ferr;
 }
 
+#ifndef FALLOC_FL_KEEP_SIZE
 #define FALLOC_FL_KEEP_SIZE    0x01
+#endif
+
+#ifndef FALLOC_FL_PUNCH_HOLE
 #define FALLOC_FL_PUNCH_HOLE   0x02
+#endif
 
 Ferr binary::discard(const Block &block) {
 	Ferr ferr = 0;
